8e2886897fef8f7a3028c1ad60e6949fa83bdf44
TheRealCelt <43344504+TheRealCelt@users.noreply.github.com>
2023-07-28 08:59:16 +0000

    Copilot second third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+//Create Web Server
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+//Create HTTP Server
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+//Define Port
+var port = process.env.PORT || 8080;
+
+//Create Database
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/comments';
+
+//Connect to Database
+mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    db.close(
